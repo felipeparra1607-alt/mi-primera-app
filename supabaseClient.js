c12a8f95b824b328a07ee693dea1838582128167
felipeparra1607-alt <felipe.parra1607@gmail.com>
2026-02-09 10:39:27 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,7 +5,7 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    persistSession: false,
-    autoRefreshToken: false,
+    persistSession: true,
+    autoRefreshToken: true,
   },
 });
